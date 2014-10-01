505005991b372d8e1f7dd669a8ef503a8970e319
David <dmeyer3691@gmail.com>
2014-10-01 10:24:39 -0400

    Created base rails app

---
+++ youdou/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Youdou::Application.config.secret_key_base = 'ce01cd6733184b17eaaa16e9c0d6c45a47dde8745e1b90c673f718c429ece9e2dfaaa353eac72035ed4865195ad812974c6a5b26936a05ea6c23e38a848cc5c4'
