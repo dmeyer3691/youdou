eb7c23446c1d5b11f69affa364fe56f4bc486bf1
Jae Lee <icarurs@gmail.com>
2014-10-24 13:50:52 -0400

    Start filling out Event page with feeds

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,4 +1,12 @@
 class EventController < ApplicationController
-  def show
+  def index
+    urls = %w[http://ohiounion.osu.edu/CentralCalendar/StudentLife.EventCalendar.Web.Service.RssHandler.ashx?d=11]
+    feeds = Feedjira::Feed.fetch_and_parse urls
+
+    feed = feeds["http://ohiounion.osu.edu/CentralCalendar/StudentLife.EventCalendar.Web.Service.RssHandler.ashx?d=11"]
+
+    @title = feed.title
+    @url = feed.url
+    @entries = feed.entries
   end
 end
