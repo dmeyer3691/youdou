3e6fbd261046088578e7b73bc46d3ae16e894e09
Jae Lee <icarurs@gmail.com>
2014-11-07 14:59:04 -0500

    Configure workers for Heroku

---
+++ app/workers/event_worker.rb
@@ -0,0 +1,22 @@
+# app/workers/event_worker.rb
+class EventWorker
+  include Sidekiq::Worker
+
+  def grab_events
+    urls = %w[http://ohiounion.osu.edu/CentralCalendar/StudentLife.EventCalendar.Web.Service.RssHandler.ashx?d=11]
+    feeds = Feedjira::Feed.fetch_and_parse urls
+    feed = feeds["http://ohiounion.osu.edu/CentralCalendar/StudentLife.EventCalendar.Web.Service.RssHandler.ashx?d=11"]
+
+    feed.entries.each do |entry|
+      url = entry.url
+      doc = Nokogiri::HTML(open(url))
+      name = entry.title
+      date = Date.parse(doc.css(".date")[0].text)
+
+      if (Event.where(name: name, date: date).empty?)
+        Event.create(name: name, date: date, url: url)
+      end
+    end
+
+  end
+end
