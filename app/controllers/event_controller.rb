9c4d56eaef972900559442d87f5ed08eb75a9fd3
Jae Lee <icarurs@gmail.com>
2014-11-07 14:58:09 -0500

    Display events on events page

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,12 +1,6 @@
+require 'open-uri'
 class EventController < ApplicationController
   def index
-    urls = %w[http://ohiounion.osu.edu/CentralCalendar/StudentLife.EventCalendar.Web.Service.RssHandler.ashx?d=11]
-    feeds = Feedjira::Feed.fetch_and_parse urls
-
-    feed = feeds["http://ohiounion.osu.edu/CentralCalendar/StudentLife.EventCalendar.Web.Service.RssHandler.ashx?d=11"]
-
-    @title = feed.title
-    @url = feed.url
-    @entries = feed.entries
+    @events = Event.all
   end
 end
