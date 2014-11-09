f4ac422a1b64886aaec4cf7a6b7e24d69fb05235
Jae Lee <icarurs@gmail.com>
2014-11-09 18:27:18 -0500

    Implement Event following

---
+++ app/models/event_relationship.rb
@@ -0,0 +1,7 @@
+class EventRelationship < ActiveRecord::Base
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "Event"
+
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
+end
