bd7c24402e600c5bffa5496e52fb6b500daec0c6
David <dmeyer3691@gmail.com>
2014-11-09 16:38:55 -0500

    Added ability to display watsons answers to answer page

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,4 +1,16 @@
+require 'json'
+
 class AnswerController < ApplicationController
-  def index
-  end
+	def index
+		config_path = File.expand_path("../rpr.py", __FILE__)
+		result = IO.popen("python3 #{config_path}")
+		string = ""
+		result.each do |line|
+			string.concat(line)
+		end
+
+		@jsonData = JSON.parse(string)
+
+		print @jsonData["title"]
+	end
 end
