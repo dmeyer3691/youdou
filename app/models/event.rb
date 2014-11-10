96afcb8a3624e34d019b2ff27a6dfaf763c59fdf
Jae Lee <icarurs@gmail.com>
2014-11-09 19:43:13 -0500

    When deleting Events also delete the Event from the User following list

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
   scope :old, -> { where("date < ?", Date.today) }
+
+  has_many :event_relationships, class_name: "EventRelationship",
+    foreign_key: "followed_id", dependent: :destroy
 end
