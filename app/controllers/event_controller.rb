da184886a223de0d8eb043b943416ab499eddf47
Jae Lee <icarurs@gmail.com>
2014-11-11 12:38:36 -0500

    Order events in ascending order by date

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,6 +1,6 @@
 class EventController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.all.order('date ASC')
 
     if user_signed_in?
       @my_events = current_user.following_events
