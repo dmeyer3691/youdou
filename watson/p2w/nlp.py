2d7a2fdd9de303d0f6a8d432b06aeb2b5b451874
ajdagokcen <avalanchebreakdown@gmail.com>
2014-11-23 16:57:19 -0500

    Adjust document handling

--- watson/p2w/nlp.py
+++ watson/p2w/nlp.py
@@ -67,7 +67,7 @@ grammar = r"""
 
 stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'many', 'part', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'let', 'go', 'none']
 
-domainspecificstopwords = ['organization', 'group', 'student', 'person', 'people', 'class', 'club', 'course']
+domainspecificstopwords = ['meet', 'meeting', 'organization', 'group', 'student', 'person', 'people', 'class', 'club', 'course']
 
 ########## functions related to parsing/chunking
 
@@ -371,6 +371,7 @@ def removeRepeats(l):
 #text = 'arabic lawyer organization'
 #text = 'and if A is useful and comes to dominate the population , then the probability of an AB individual appearing then also tends towards 100 % .'
 #text = 'The apostles may have believed that Jesus walked on water: that does NOT make it true.'
+#text = 'Where does the anime club meet?'
 
 #kws = nps(text)
 #syns = addSyns(kws)
