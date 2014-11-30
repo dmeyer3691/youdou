cfe7846e4987a0e9feac132c6a24a96c5109b698
Jae Lee <icarurs@gmail.com>
2014-11-29 22:15:23 -0500

    Specify defaults and validations for Interest images

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -5,4 +5,12 @@ class Interest < ActiveRecord::Base
   before_save { |interest| interest.name = interest.name.downcase }
 
   acts_as_followable
+
+  # Paperclip
+  has_attached_file :image, :styles => {
+    :medium => "300x300#",
+    :thumb => "100x100#"
+  }, default_url: "http://placehold.it/300x300"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 end
