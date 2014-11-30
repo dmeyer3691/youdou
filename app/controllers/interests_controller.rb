ee044504bc160d41f9aa790bbebe8f35cb4e40be
Jae Lee <icarurs@gmail.com>
2014-11-30 00:35:22 -0500

    Fix Interests image form

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -16,9 +16,19 @@ class InterestsController < ApplicationController
     redirect_to profile_path
   end
 
+  def update
+    @interest = Interest.find(params[:id])
+
+    if @interest.update_attributes(interest_params)
+      redirect_to interests_path
+    else
+      redirect_to interests_path
+    end
+  end
+
   private
 
   def interest_params
-    params.require(:interest).permit(:name)
+    params.require(:interest).permit(:name, :image)
   end
 end
