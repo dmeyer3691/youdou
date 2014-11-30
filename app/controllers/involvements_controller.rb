6d762faba5472aeafb87878d5601e0ff757622f9
Jae Lee <icarurs@gmail.com>
2014-11-30 00:57:15 -0500

    Allow image uploading for Involvements

--- app/controllers/involvements_controller.rb
+++ app/controllers/involvements_controller.rb
@@ -1,4 +1,8 @@
 class InvolvementsController < ApplicationController
+  def index
+    @involvements = Involvement.all
+  end
+
   def create
     @involvement = Involvement.find_or_create_by(involvement_params)
     current_user.follow(@involvement)
@@ -12,10 +16,20 @@ class InvolvementsController < ApplicationController
     redirect_to profile_path
   end
 
+  def update
+    @involvement = Involvement.find(params[:id])
+
+    if @involvement.update_attributes(involvement_params)
+      redirect_to involvements_path
+    else
+      redirect_to involvements_path
+    end
+  end
+
   private
 
   def involvement_params
-    params.require(:involvement).permit(:name)
+    params.require(:involvement).permit(:name, :image)
   end
 end
 
